2b5d1986fb45ec7f2ed96b121dcce22b125a02f7
Esther Camacho <Esther.Camacho@alu.uclm.es>
2022-11-18 00:18:44 +0100

    Página de inicio creada, no va el efecto de máquina de escribir

--- js/script.js
+++ js/script.js
@@ -1,8 +1,18 @@
 /* MENU DESPLEGABLE */ 
+const header = document.querySelector("header");
+window.addEventListener ("scroll", function() {
+	header.classList.toggle ("sticky", window.scrollY > 100);
+});
+
 let menu = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.navbar');
+let navlist = document.querySelector('.navlist');
 
 menu.onclick = () => {
-    menu.classList.toggle('bx-x');
-    navbar.classList.toggle('open');
-}
+	menu.classList.toggle('bx-x');
+	navlist.classList.toggle('open');
+};
+
+window.onscroll = () => {
+	menu.classList.remove('bx-x');
+	navlist.classList.remove('open');
+};
