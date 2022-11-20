723bb4dc49747a36b8119d7f1d1b4e9c637a03ec
Esther Camacho <Esther.Camacho@alu.uclm.es>
2022-11-20 17:38:43 +0100

    Añadir tema oscuro o claro

--- js/script.js
+++ js/script.js
@@ -18,3 +18,32 @@ window.onscroll = () => {
 };
 
 
+/****** MODO OSCURO ********/
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'bx-sun'
+
+//Previously selected topic (if user selected)
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+//We obtain the current theme that the interface has by validating the dark-theme class
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'ligth'
+const getCurrentIcon = () => document.classList.contains(iconTheme) ? 'bx-moon' : 'bx-sun'
+
+//We validate if the user previously chose a topic
+if(selectedTheme){
+	//If the validation is fullfilled, we ask the issue was to know if we activated or desactivated the dark mode
+	document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+	themeButton.classList[selectedIcon === 'bx-moon' ? 'add' : 'remove'](iconTheme)
+}
+
+//Activate / desactivate the theme manually with the button
+themeButton.addEventListener('click', () => {
+	//Add or remove the dark/icon theme
+	document.body.classList.toggle(darkTheme)
+	themeButton.classList.toggle(iconTheme)
+	//We save the theme and the current icon that the user chose
+	localStorage.setItem('selected-theme', getCurrentTheme())
+	localStorage.setItem('selected-icon', getCurrentIcon())
+})
